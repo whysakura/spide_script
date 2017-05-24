c90e68aefb2be6881630df48b39816982e1d2c88
weiruduan <wrd@putao.cn>
2017-05-24 15:21:43 +0800

    proxies_list

--- spide_check.py
+++ spide_check.py
@@ -17,6 +17,7 @@ mylog = Logger(chkip_logging_filename)
 def insert_proxies(proxies_list):
     if not proxies_list:
         return
+    proxies_list = [proxies_list]
     con = MyPyMysql(**mysql_config)
     sql = """ replace into pt_db.spide_proxies_ip (proxy_host,proxy_port,address) values %s """
     con.insert_query(sql, proxies_list)
@@ -59,9 +60,9 @@ class AsySpider(object):
                     mylog.info('proxy_ip_list队列无值,等待添加中....')
                 i = json.loads(self.r.blpop("proxy_check_ip_list", timeout=0)[1])
                 httpconfigs = get_ip_http_config()
-                httpconfigs['proxy_host'] = i['proxy_host']
-                httpconfigs['proxy_port'] = int(i['proxy_port'])
-                yield Spide(self.url, **httpconfigs).async_proxy()
+                # httpconfigs['proxy_host'] = i['proxy_host']
+                # httpconfigs['proxy_port'] = int(i['proxy_port'])
+                # yield Spide(self.url, **httpconfigs).async_proxy()
                 # yield Spide(self.url, **httpconfigs).async()
             except Exception as e:
                 mylog.error(str(e))
