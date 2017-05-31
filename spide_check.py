b8f32fa78da51350a30aab1c961c00c356d2a5cf
weiruduan <wrd@putao.cn>
2017-05-31 10:00:59 +0800

    consumer producer

--- spide_check.py
+++ spide_check.py
@@ -57,7 +57,7 @@ class AsySpider(object):
                     insert_proxies(insert_lists)
                     insert_lists = []
                     mylog.info('proxy_ip_list队列无值,等待添加中....')
-                i = json.loads(self.r.blpop("proxy_check_ip_list", timeout=0)[1])
+                i = json.loads(self.r.blpop("proxy_check_ip_list", timeout=100)[1])
                 httpconfigs = get_ip_http_config()
                 httpconfigs['proxy_host'] = i['proxy_host']
                 httpconfigs['proxy_port'] = int(i['proxy_port'])
