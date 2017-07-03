6d13a58cbb21f9e9e2840ca26f882664b16a9a9e
weiruduan <weiruduan@guanmai.cn>
2017-07-03 10:06:59 +0800

    setting

--- setting.py
+++ setting.py
@@ -68,8 +68,8 @@ second_url = 'http://www.coobobo.com/free-http-proxy/'
 third_url = 'http://www.httpdaili.com/mfdl/'
 ip_logging_filename = os.path.join((os.path.dirname(__file__)), "logger",'ips.log')
 chkip_logging_filename = os.path.join((os.path.dirname(__file__)), "logger",'check_ips.log')
-page_num = 4 # 取5页
-sleep_tims = 1200 # 睡眠300s
+page_num = 2 # 取5页
+sleep_tims = 2400 # 睡眠300s
 check_process = 3
 ip_http_config = {
     "headers": {
